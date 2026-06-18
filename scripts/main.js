cdc506d27106f003a44308e4e67b6023c6598ec7
Daniel Chernov <chernov.dani@gmail.com>
2026-06-18 22:37:04 +0300

    add profile loading functionality and improve profile selection UI

--- scripts/main.js
+++ scripts/main.js
@@ -7,12 +7,22 @@ const model = Model()
 const generateButton = document.querySelector("#generateButton")
 const loadButton = document.querySelector("#loadButton")
 const saveButton = document.querySelector("#saveButton")
+const profileOptionsContainer = document.querySelector(".select-profile-container")
 
 
 let currentUser = null
 
-const saveUsersToLocalStorage = function(){
+const getUsersFromLocalStorage = function(){
+    const users = localStorage.getItem("users")
 
+    if(!users){
+        return {}
+    }
+
+    return JSON.parse(users)
+}
+const saveUsersToLocalStorage = function(users){
+    localStorage.setItem("users", JSON.stringify(users))
 }
 
 
@@ -40,7 +50,13 @@ saveButton.addEventListener("click", () => {
         return
     }
 
-    localStorage.setItem("savedProfile", JSON.stringify(currentUser))
+    const users = getUsersFromLocalStorage()
+    const userName = `${currentUser.mainUser.firstName} ${currentUser.mainUser.lastName}`
+
+    users[userName] = currentUser
+
+    saveUsersToLocalStorage(users)
+
     renderer.renderSuccsess("Saved Successfuly")
     setTimeout(()=>{
         renderer.clearError()
@@ -50,17 +66,35 @@ saveButton.addEventListener("click", () => {
 loadButton.addEventListener("click", () => {
     renderer.clearError()
 
-    const savedProfile = localStorage.getItem("savedProfile")
-
-    if(!savedProfile){
-        renderer.renderError("No saved profile found.")
+    if(!profileOptionsContainer.classList.contains("hidden")){
+        renderer.hideProfileOptions()
         return
     }
 
-    const parsedProfile = JSON.parse(savedProfile)
+    const users = getUsersFromLocalStorage()
+    const profileNames = Object.keys(users)
 
-    currentUser = parsedProfile
+    renderer.showProfileOptions(profileNames)
+})
+
+profileOptionsContainer.addEventListener("click", (event) => {
+    if(!event.target.classList.contains("profile-option")){
+        return
+    }
+
+    const selectedProfileName = event.target.dataset.profileName
+
+    const users = getUsersFromLocalStorage()
+    const selectedProfile = users[selectedProfileName]
+
+    if(!selectedProfile){
+        renderer.renderError("Selected profile was not found.")
+        return
+    }
+
+    currentUser = selectedProfile
     renderer.render(currentUser)
+    renderer.hideProfileOptions()
 
     renderer.renderSuccsess("Profile loaded successfully.")
     setTimeout(()=>{
