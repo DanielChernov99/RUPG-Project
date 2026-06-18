abe74933e5618e5b4236bac930aa98e836a11346
Daniel Chernov <chernov.dani@gmail.com>
2026-06-18 20:00:01 +0300

    Add RandomPokemonService to fetch random Pokémon data

---
+++ scripts/Services/randomPokemonService.js
@@ -0,0 +1,20 @@
+
+const RandomPokemonService = async function(){
+
+    const getRandomPokemon = async function(){
+        try{
+            const response = await fetch("")
+        }
+        catch{
+
+        }
+       
+    }
+
+    return{
+        getRandomPokemon
+    }
+
+}
+
+export default RandomPokemonService
